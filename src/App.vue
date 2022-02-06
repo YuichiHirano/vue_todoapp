dc6b0c74cb1ca502337657c68421933c8d9808a5
yhirano <23518736+YuichiHirano@users.noreply.github.com>
2022-02-06 15:57:03 +0000

    create task list

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Tasks></Tasks>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Tasks from "./components/Tasks.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Tasks,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>タスクリスト</h2>
+    <ul>
+      <li v-for="task in tasks" :key="task.id">
+        <input type="checkbox" v-model="task.completed" />
+        <span v-text="task.content"></span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tasks: [
+        { id: 0, content: "HTML", completed: false },
+        { id: 1, content: "CSS", completed: false },
+        { id: 2, content: "JavaScript", completed: false },
+        { id: 3, content: "Vue.js", completed: false },
+      ],
+    };
+  },
+};
+</script>
